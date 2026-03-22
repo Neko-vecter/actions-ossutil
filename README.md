11cf610d4c78d8749fcb1fa4cfd02b773c7c0708
Neko-vecter <vecterfang@icloud.com>
2026-03-22 03:53:07 -0400

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# actions-ossutil
+
+使用 Node 24 的阿里云（Aliyun）ossutil 工具
+
+```yml
+steps:
+  - uses: actions/checkout@v6
+
+  - uses: Neko-vecter/actions-ossutil@v1
+    with:
+      endpoint: ${{ secrets.OSS_ENDPOINT }}.aliyuncs.com
+      access-key-id: ${{ secrets.OSS_ACCESS_KEY_ID }}
+      access-key-secret: ${{ secrets.OSS_ACCESS_KEY_SECRET }}
+      sts-token: ${{ secrets.OSS_ACCESS_STS_TOKEN }}
+  - run: ossutil cp -rf ./build oss://bucket/path
+```
---
+++ main.js
@@ -0,0 +1,54 @@
+import * as core from "@actions/core";
+import * as exec from "@actions/exec";
+import * as toolCache from "@actions/tool-cache";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// 获取 __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const url = "http://gosspublic.alicdn.com/ossutil/1.7.7/ossutil64";
+
+async function main() {
+    const ENDPOINT = core.getInput("endpoint");
+    const ACCESS_KEY_ID = core.getInput("access-key-id");
+    const ACCESS_KEY_SECRET = core.getInput("access-key-secret");
+    const STS_TOKEN = core.getInput("sts-token");
+
+    let toolPath = toolCache.find("ossutil", "1.7.7");
+
+    if (!toolPath) {
+        core.info(`downloading from ${url}`);
+        toolPath = await toolCache.downloadTool(url);
+        core.info(`downloaded to ${toolPath}`);
+    }
+
+    const bin = path.join(__dirname, ".bin");
+    if (!fs.existsSync(bin)) {
+        fs.mkdirSync(bin, { recursive: true });
+    }
+
+    fs.copyFileSync(toolPath, path.join(bin, "ossutil"));
+    fs.chmodSync(path.join(bin, "ossutil"), 0o755);
+
+    core.addPath(bin);
+
+    await exec.exec("ossutil", [
+        "config",
+        "-e",
+        ENDPOINT,
+        "-i",
+        ACCESS_KEY_ID,
+        "-k",
+        ACCESS_KEY_SECRET,
+        ...(STS_TOKEN ? ["-t", STS_TOKEN] : []),
+        "-L",
+        "CH"
+    ]);
+}
+
+main().catch(error => {
+    core.setFailed(error.message);
+});
